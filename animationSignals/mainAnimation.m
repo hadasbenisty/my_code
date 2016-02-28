e2566d7fb7fd5fd0ea74ccc3743c9ebbb3151350
hadas <hadas.benisty@gmail.com>
2016-02-28 09:54:48 +0200

    this is main that need some more work

--- animationSignals/mainAnimation.m
+++ animationSignals/mainAnimation.m
@@ -89,6 +89,13 @@ params  = SetGenericQuestParamsAnimation;
 % ms.row_emd.beta=.5;
 % params.col_emd.beta=.5;
 % params.trial_emd.beta=.5;
+col_init_aff = feval(params.init_aff_col.initAffineFun, data, params.init_aff_col);
+trial_init_aff = feval(params.init_aff_col.initAffineFun, permute(data, [2 1 3]), params.init_aff_col);
+row_init_aff = feval(params.init_aff_col.initAffineFun, permute(data, [1 3 2]), params.init_aff_col);
+[ err_rate2,organized_data2,row_order2,col_order2,orderTime2 ] = OrganizeData3D( dataOrig, data, trial_init_aff, col_init_aff,...
+row_init_aff, shuffleRows, shuffleCols, shuffleTime, 4, 3, 4 );
+
+
 
 [ col_tree, trial_tree, row_tree,  col_dual_aff, trial_dual_aff, row_dual_aff] = RunGenericQuestionnaire3D( params, data );
 [ err_rate1,organized_data1,row_order1,col_order1,orderTime1 ] = OrganizeData3D( dataOrig, data, col_dual_aff, trial_dual_aff,...
@@ -96,7 +103,6 @@ row_dual_aff, shuffleRows, shuffleCols, shuffleTime, 4, 3, 4 );
 orderCols1 = getOrder(col_tree{2}.clustering);
 orderRows1 = getOrder(trial_tree{2}.clustering);
 dataReordered1 = uint8(data(fliplr(orderRows),orderCols,orderTime1));
-implay(uint8(dataReordered1));
 
 implay(uint8(organized_data1));
 implay(uint8(organized_data));
@@ -109,3 +115,25 @@ organized_data111 = organized_data111(:,:, orderTime1);
 [row_vecs, row_vals] = CalcEigs(col_dual_aff, 3);
 v = [ones(1,104) 100*ones(1,232)];
 figure;PlotEmbedding(row_vecs*row_vals, v(shuffleRows), 'Row Embedding');
+
+
+
+movDataOrig = matrix2mov(dataOrig);movie2avi(movDataOrig, 'orig.avi');
+movPerm = matrix2mov(data);movie2avi(movPerm, 'perm.avi');
+movSVMclust = matrix2mov(dataReordered);movie2avi(movSVMclust, 'PCAclust.avi');
+movTDtrees = matrix2mov(organized_data1);movie2avi(movTDtrees, 'TdPcaTrees.avi');
+
+
+MOV1 = aviread('TdPcaTrees.avi');
+
+
+vidObj = VideoWriter('TdPcaTrees.avi');
+    open(vidObj);
+ 
+    for k = 1:length(MOV1)
+       
+       writeVideo(vidObj,MOV1(k));
+    end
+  
+    % Close the file.
+    close(vidObj);
