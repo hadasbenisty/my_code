28845d6552996c00d6f887952f0170efc58a100f
hadasbenisty <hadas.benisty@gmail.com>
2016-02-29 08:28:51 +0200

    plot selected time frames

--- gen_utils/plotByClustering.m
+++ gen_utils/plotByClustering.m
@@ -1,13 +1,15 @@
-function plotByClustering(meanMat, allMat, ttl)
+function plotByClustering(meanMat, allMat, ttl, timeFrames)
 
-
+if nargin == 3
+    timeFrames = 1:size(meanMat, 2);
+end
 figure;
 subplot(1,2,1);
-imagesc(meanMat);
+imagesc(meanMat(:, timeFrames));
 set(gca, 'Ytick', 1:size(meanMat,2));title(sprintf('%s\nMean Image', ttl));
 for k=1:length(allMat)
    subplot(length(allMat), 2, 2*k);
-   imagesc(allMat{k});
+   imagesc(allMat{k}(:,timeFrames));
    set(gca, 'XtickLabel',{});
    set(gca, 'YtickLabel',{});
    colorbar;
