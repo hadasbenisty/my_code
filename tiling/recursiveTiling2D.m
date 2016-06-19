3b4a5a2c8815aaf99ba60a8152b6638b36f3424c
hadasbenisty <hadas.benisty@gmail.com>
2016-06-19 10:05:46 +0300

    f

--- tiling/recursiveTiling2D.m
+++ tiling/recursiveTiling2D.m
@@ -1,12 +1,13 @@
 function [minErr, tiling, solutionTiling] = recursiveTiling2D(data, row_tree, col_tree, vol, ind2data, tiling, solutionTiling, minErr)
 
-
+params.beta_row=0;
+params.beta_col=0;
 MAX_S = numel(data);
 if ind2data > MAX_S ||  sum(tiling.isbusy(:) == 0) == 0
-    currErr = evalTilingErr(data, tiling);
+    currErr = evalTilingErr(data, tiling, params);
     if currErr < minErr
         minErr =  currErr;
-        disp(['Volume = ' num2str(vol) ' err = ' num2str(minErr)]);
+%         disp(['Volume = ' num2str(vol) ' err = ' num2str(minErr)]);
 %         disp(tiling.isbusy);    
         disp('---------------------');
         solutionTiling = tiling;
@@ -20,6 +21,25 @@ if tiling.isbusy(row_i, col_i) % check if it's busy and shift to the next one
 end
 
 tilesList = getTiles(vol);
+% tilesList(1).row=10;
+% tilesList(1).col=70;
+% tilesList(2).row=20;
+% tilesList(2).col=30;
+% tilesList(3).row=60;
+% tilesList(3).col=10;
+% 
+% tilesList(4).row=40;
+% tilesList(4).col=40;
+% 
+% tilesList(5).row=40;
+% tilesList(5).col=20;
+% 
+% tilesList(6).row=20;
+% tilesList(6).col=20;
+% 
+% tilesList(7).col=20;
+% tilesList(7).col=20;
+% tilesList=tilesList(1:7);
 for ti = 1:length(tilesList)
     % cheking if this tile fits
     isTileFits = checkTile(data, row_tree, col_tree, ind2data, tilesList(ti));
@@ -30,16 +50,16 @@ for ti = 1:length(tilesList)
     isTileNotOcc = checkTileOcc(tiling, ind2data, tilesList(ti));
     if isTileNotOcc
         % marking this tile 
-        tiling = markTile(tiling, tilesList(ti), ind2data);
+        tiling = markTile(tiling, tilesList(ti), ind2data, 2);
         % see if we don't already get an error higher than the prev. so
         % there is a point to continue
-        currErr = evalTilingErr(data, tiling);
+        currErr = evalTilingErr(data, tiling, params);
         if currErr < minErr
             [minErr, tiling, solutionTiling] = recursiveTiling2D(data, row_tree, col_tree, vol, ind2data+1, tiling, solutionTiling, minErr);
         end
         % remove this tile and continue to examine the other tiles in the
         % list
-        tiling = unmarkTile(tiling, tilesList(ti), ind2data);
+        tiling = unmarkTile(tiling, tilesList(ti), ind2data, 2);
     end
 end
 end
