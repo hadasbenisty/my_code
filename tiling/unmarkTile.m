25696a83a9a2094bdc7a07aa64f09a6f807a99c1
hadasbenisty <hadas.benisty@gmail.com>
2016-06-19 10:04:58 +0300

    add verbose

--- tiling/unmarkTile.m
+++ tiling/unmarkTile.m
@@ -1,10 +1,12 @@
-function tiling = unmarkTile(tiling, tile, ind2data)
+function tiling = unmarkTile(tiling, tile, ind2data, verbose)
 % disp(['Unmarking tile ' num2str(tile.row) ' by ' num2str(tile.col) ' in ' num2str(ind2data)]);
 
 [row_i,col_i] = ind2sub(size(tiling.isbusy),ind2data);
 tiling.isLeader(row_i,col_i) = false;
 tiling.isbusy(row_i:row_i+tile.row-1, col_i:col_i+tile.col-1) = 0;
-imagesc(tiling.isbusy);title(['Tiling Vol = ' num2str(tile.row*tile.col)]);
-drawnow;
-
+if verbose > 1
+    imagesc(tiling.isbusy);
+%     title(['Tiling Vol = ' num2str(tile.row*tile.col)]);
+    drawnow;
+end
 end
